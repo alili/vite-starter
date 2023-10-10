c4d9749b8fc7724df8a62bf5edf6e4919c364ca4
nemo <122499469@qq.com>
2023-10-10 14:17:49 +0800

    feat: add auto-imports.d.ts & components.d.ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,14 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    './auto-imports.d.ts',
+    './components.d.ts',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
 }
