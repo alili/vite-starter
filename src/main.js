6c680228bac71607ec286a1ff412b06df5ceb5fa
nemo <122499469@qq.com>
2023-10-10 14:18:17 +0800

    feat(main): add icons & dark theme

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import ArcoVueIcon from '@arco-design/web-vue/es/icon'
 import './assets/main.css'
 
 import { createApp } from 'vue'
@@ -6,9 +7,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import '@arco-design/web-vue/dist/arco.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ArcoVueIcon)
 
 app.mount('#app')
+
+let prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+if (prefersDarkMode) {
+  document.body.setAttribute('arco-theme', 'dark')
+}
