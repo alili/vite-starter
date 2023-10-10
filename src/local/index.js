d9e60e17acb133dd441d19ccec91ac36198cbc84
nemo <122499469@qq.com>
2023-10-10 16:03:17 +0800

    feat: change default lang

--- src/local/index.js
+++ src/local/index.js
@@ -2,7 +2,7 @@ import { createI18n } from 'vue-i18n'
 import en from './en-US'
 import cn from './zh-CN'
 
-const defaultLocale = 'en-US'
+const defaultLocale = 'zh-CN'
 
 const i18n = createI18n({
   locale: defaultLocale,
