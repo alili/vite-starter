1b6401980b65868406ac69f1bc7c346f0c0f6b84
nemo <122499469@qq.com>
2023-11-08 23:50:25 +0800

    fix: typo

--- src/store/counter.js
+++ src/store/counter.js
@@ -1,7 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
+const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
   function increment() {
@@ -10,3 +10,5 @@ export const useCounterStore = defineStore('counter', () => {
 
   return { count, doubleCount, increment }
 })
+
+export default useCounterStore
